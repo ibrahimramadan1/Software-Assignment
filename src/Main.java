6f8bff226d3a35480084cd716703d42cb178d47e
Muhammad El Kassas <61469701+MuhammadElKassas@users.noreply.github.com>
2020-02-28 16:24:15 +0200

    20170213-added SphereVolume Class in SubscriberArr

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
-			new twoPowerN(),new SummationSeries()
+			new twoPowerN(),new SummationSeries(),new SphereVolume
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
