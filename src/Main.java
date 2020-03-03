ffa53c537227c44c90fba849b46bbef16cb40797
nairamagdyy <36415970+nairamagdyy@users.noreply.github.com>
2020-03-03 15:29:30 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
-			new twoPowerN(),new SphereCircumference(),new SummationSeries(),new SphereVolume(), new Fibonacci(),new SphereArea()
-	};
+		  new twoPowerN(),new SphereCircumference(),new SummationSeries(),new SphereVolume(), new Fibonacci(),new SphereArea(), new BallVolume()
+			};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
 		for (ISubscriber sub : subscribers) {
